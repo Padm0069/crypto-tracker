053ccd2a2a79a8cf153d7f7c2b54b2fa8e2c8890
Padm Keshav <kesavpadm2003@outlook.com>
2023-08-01 22:56:30 +0530

    Adding charts and table

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "react-alice-carousel/lib/alice-carousel.css";
+import CryptoContext from "./CryptoContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CryptoContext>
+      <App />
+    </CryptoContext>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
